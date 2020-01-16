7c482a9d941e92e76b3fe56941bcaf94123441d7
Ryan-Yeo <ryanrj@hotmail.sg>
2020-01-16 13:31:34 +0800

    done part 2

--- dummy-app/src/utils/i18n.js
+++ dummy-app/src/utils/i18n.js
@@ -1,3 +1,24 @@
 export function getCurrentLocale(url) {
   // your code here. have fun!
+
+  // Substrings
+  const en = "en";
+  const zh = "zh-cn";
+
+  // Test 1
+  if(url.indexOf(en) === -1 && url.indexOf(zh) === -1){
+    return "en";
+
+  // Test 2
+  } else if (url.indexOf(en) === 1){
+    return "en";
+
+  // Test 3
+  } else if (url.indexOf(zh) === 1){
+    return "zh-cn";
+
+  // Test 4
+  } else if (url.indexOf(en) !== 1 || url.indexOf(zh) !== 1){
+    return "en";
+  }
 }
